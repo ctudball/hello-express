c70fc736546ed42eeeb555c56f6c2e6ade8e9a43
Cameron Tudball <ctudball@outlook.com>
2018-08-23 16:54:57 +1000

    Add inital endpoints

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const server = express();
 const port = 3000;
 
 
+// Create main endpoint
+server.get('/', (request, response) =>{
+    response.send('<h1>Hello from Express</h1>')
+});
+
+// Create heath endpoint
+server.get('/health', (req, res) => {
+    res.contentType('text/plain');
+    res.send("OK");
+});
+
+// Create metadata endpoint
+let metadata = '';
+server.get('/metadata', (req, res) => {
+    res.contentType('application/json');
+    res.send(metadata);
+});
+
 // Start Server
 server.listen(port);
 
